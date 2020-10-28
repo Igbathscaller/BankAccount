e0bd53cf7f2309aa516aab3462e1df7f63e806f4
Igbathscaller <Igbathscaller@gmail.com>
2020-10-28 00:26:51 -0400

    testing useless function password

--- Tester.java
+++ Tester.java
@@ -1,6 +1,11 @@
 public class Tester {
     public static void main(String[]args){
         BankAccount David = new BankAccount (219316767, "AbhijeetSaha");
-        System.out.println(David.toString);
+        System.out.println(David.toString());
+        David.deposit(123456789);
+        David.deposit(12342.23);
+        System.out.println(David.toString());
+        David.setPassword("this doesn't even matter since you can't even check it");
+        System.out.println(David.toString());
     }
 }
