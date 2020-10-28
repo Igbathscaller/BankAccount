e713813949f52654d2f54bfde1768746897f1771
Igbathscaller <Igbathscaller@gmail.com>
2020-10-28 00:02:50 -0400

    getBalance

---
+++ BankAccount.java
@@ -0,0 +1,18 @@
+public class BankAccount{
+
+    private double balance;
+    private int accountID;
+    private String password;
+    
+    public BankAccount(int acc, String pass){
+        balance = 0;
+        accountID = acc;
+        password = pass;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+
+}
