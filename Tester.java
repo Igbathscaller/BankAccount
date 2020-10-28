50c22b9d8ce38ef0a4f4a1fc11cdf9a66a93b79e
Igbathscaller <Igbathscaller@gmail.com>
2020-10-28 17:24:17 -0400

    getBalance

--- Tester.java
+++ Tester.java
@@ -7,6 +7,7 @@ public class Tester {
         System.out.println(David.toString());
         David.withdraw(12342.23);
         System.out.println(David.toString());
+        System.out.println(David.getBalance());
         System.out.println(David.toString()+David.deposit(12342.23));
         David.setPassword("this doesn't even matter since you can't even check it");
         System.out.println(David.toString());
