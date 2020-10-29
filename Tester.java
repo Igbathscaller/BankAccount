0401c7cf00242041272c08043e20b19015b7e0fc
Igbathscaller <Igbathscaller@gmail.com>
2020-10-29 12:52:10 -0400

    fix

--- Tester.java
+++ Tester.java
@@ -8,7 +8,7 @@ public class Tester {
         David.withdraw(12342.23);
         System.out.println(David.toString());
         System.out.println(David.getBalance());
-        System.out.println(David.getAccount());
+        System.out.println(David.getAccountID());
         System.out.println(David.toString()+David.deposit(12342.23));
         David.setPassword("this doesn't even matter since you can't even check it");
         System.out.println(David.toString());
